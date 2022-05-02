97067ab41033089d40aa08c98da397283e44e644
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-05-02 17:42:53 +0300

    jsx

--- my-app/src/main/ui/Login/Login.tsx
+++ my-app/src/main/ui/Login/Login.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import s from './Login.module.css'
+import classes from "../Header/Header.module.css";
+import {Path} from "../Routes/Routes";
+import {NavLink} from "react-router-dom";
 
 export const Login = () => {
+    let getActiveStyle = ({isActive}: { isActive: boolean }) => isActive ? classes.active : ''
+
     return (
         <div className={s.mainblock}>
             <div className={s.loginbox}>
                 <h1>it-incubator</h1>
-                <form className={s.loginbox_form} >
-                    <input type="text" placeholder="Email"/>
-                    <input type='text' placeholder="password"/>
-                    <span>Forgot Password</span>
-                    <button>Login</button>
+                <h3>Sign in</h3>
+                <form className={s.loginbox_form}>
+                    <input className={s.input} type="text" placeholder="Email"/>
+                    <input className={s.input} type='text' placeholder="password"/>
+                    <span className={s.forgText} >Forgot Password?</span>
+                    <button className={s.loginbox_log}>Login</button>
+
                 </form>
                 <h6>Don't have account?</h6>
-                <button>Sign Up</button>
+                <NavLink to={Path.Registration} className={getActiveStyle}>
+                    <button>Sign Up</button>
+                </NavLink>
             </div>
         </div>
     )
