620980a2106ef2747a10ded6ffc7a9e9a3d6971d
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-05-03 13:49:57 +0300

    state

--- my-app/src/main/ui/Login/Login.tsx
+++ my-app/src/main/ui/Login/Login.tsx
@@ -1,22 +1,62 @@
-import React from "react";
+import React, {ChangeEvent, FormEventHandler, useEffect, useRef, useState} from "react";
 import s from './Login.module.css'
 import classes from "../Header/Header.module.css";
 import {Path} from "../Routes/Routes";
 import {NavLink} from "react-router-dom";
+import {log} from "util";
 
 export const Login = () => {
+
     let getActiveStyle = ({isActive}: { isActive: boolean }) => isActive ? classes.active : ''
 
+
+    let [email,setEmail] = useState("")
+    let [password,setPassword] = useState("")
+
+   const handleSubmit=  (event: React.FormEvent<HTMLFormElement>) => {
+       event.preventDefault();
+   }
+const chengEmailHandler = (e:ChangeEvent<HTMLInputElement>)=>{
+    setEmail(e.currentTarget.value)
+}
+    const chengPasswordHandler = (e:ChangeEvent<HTMLInputElement>)=> {
+        setPassword(e.currentTarget.value)
+    }
+  const onClickLoginHandler = () =>{
+      console.log({
+           email,
+          password
+      })
+  }
     return (
         <div className={s.mainblock}>
             <div className={s.loginbox}>
                 <h1>it-incubator</h1>
                 <h3>Sign in</h3>
-                <form className={s.loginbox_form}>
-                    <input className={s.input} type="text" placeholder="Email"/>
-                    <input className={s.input} type='text' placeholder="password"/>
+                <form className={s.loginbox_form} onSubmit={handleSubmit}>
+                    <input className={s.input}
+
+                           type="text"
+                           placeholder="Email"
+                           // onChange={(e)=>setEmail(e.target.value)}
+                           onChange={chengEmailHandler}
+                           value={email}
+
+
+                    />
+                    <input className={s.input}
+
+                           type='password'
+                           placeholder="Password"
+                           onChange={chengPasswordHandler}
+                           value={password}
+
+
+                    />
                     <span className={s.forgText} >Forgot Password?</span>
-                    <button className={s.loginbox_log}>Login</button>
+                    <button className={s.loginbox_log}
+                            onClick={onClickLoginHandler}
+                    >Login</button>
 
                 </form>
                 <h6>Don't have account?</h6>
